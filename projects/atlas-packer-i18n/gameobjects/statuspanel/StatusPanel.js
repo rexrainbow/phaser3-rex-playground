4216f1293a4f2be02a210f3f6c2ef62c428d5630
Rex <binhua.tein@gmail.com>
2023-02-27 19:51:27 +0800

    Translate status string

--- projects/atlas-packer-i18n/gameobjects/statuspanel/StatusPanel.js
+++ projects/atlas-packer-i18n/gameobjects/statuspanel/StatusPanel.js
@@ -1,8 +1,24 @@
 import { SimpleLabel } from '../../../../../phaser3-rex-notes/templates/ui/ui-components.js';
+import TextTranslation from '../../../../../phaser3-rex-notes/plugins/texttranslation.js';
 
 class StatusPanel extends SimpleLabel {
+    constructor(scene, config) {
+        super(scene, config);
+
+        this.resetDisplayContent();
+
+        this.translation = new TextTranslation(this, {
+            translationKey: 'status'
+        });
+    }
+
     showStatus(mainPanel) {
-        this.resetDisplayContent(`${mainPanel.imageCount} images, ${mainPanel.textureWidth} x ${mainPanel.textureHeight}`);
+        this.translation
+            .updateInterpolation('imageCount', mainPanel.imageCount)
+            .updateInterpolation('textureWidth', mainPanel.textureWidth)
+            .updateInterpolation('textureHeight', mainPanel.textureHeight)
+            .updateText();
+
         return this;
     }
 }
