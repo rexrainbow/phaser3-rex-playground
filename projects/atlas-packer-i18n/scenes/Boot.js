feb896af1d7b3f4647b368ea9488fd6eba3999e7
Rex <binhua.tein@gmail.com>
2023-03-01 09:43:54 +0800

    Fix lng

--- projects/atlas-packer-i18n/scenes/Boot.js
+++ projects/atlas-packer-i18n/scenes/Boot.js
@@ -43,7 +43,7 @@ var SetupI18Next = function (scene) {
         i18next
             .use(Backend)
             .init({
-                lng: 'zh-tw',  // 'en', 'zh-tw'
+                lng: 'zh-TW',  // 'en', 'zh-TW'
                 ns: 'ui',
                 debug: true,
                 backend: {
