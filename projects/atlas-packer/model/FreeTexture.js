61f4e5b9fb5107be5a25fcba3e6428432a1905e6
Rex <binhua.tein@gmail.com>
2023-03-23 12:13:44 +0800

    Fix bug

--- projects/atlas-packer/model/FreeTexture.js
+++ projects/atlas-packer/model/FreeTexture.js
@@ -1,23 +1,41 @@
 import GetCache from '../../../../phaser3-rex-notes/plugins/utils/system/GetCache.js';
 
-var FreeTexture = function (scene, imageData) {
-    var cache = GetCache(scene, 'image');
+var FreeTexture = function (scene, imageDataArray) {
+    if (!Array.isArray(imageDataArray)) {
+        imageDataArray = [imageDataArray]
+    }
 
-    if (!Array.isArray(imageData)) {
-        var key = imageData.key;
-        if (cache.exists(key)) {
-            cache.remove(key);
-        }
-    } else {
-        var imageDataArray = imageData;
-        for (var i = 0, cnt = imageDataArray.length; i < cnt; i++) {
-            var key = imageDataArray[i].key;
+    var cache = GetCache(scene, 'image');
+    for (var i = 0, cnt = imageDataArray.length; i < cnt; i++) {
+        var imageData = imageDataArray[i],
+            key = imageData.key,
+            frameName = imageData.frame;
+        if (!frameName) {
             if (cache.exists(key)) {
                 cache.remove(key);
             }
+
+        } else {
+            var texture = cache.get(key);
+            texture.remove(frameName);
+            if (!HasAnyFrame(texture)) {
+                cache.remove(key);
+            }
         }
+
     }
 
+}
+
+var HasAnyFrame = function (texture) {
+    var frames = texture.frames;
+    for (var name in frames) {
+        if (name === '__BASE') {
+            continue;
+        }
+        return true;
+    }
+    return false;
 }
 
 export default FreeTexture;
