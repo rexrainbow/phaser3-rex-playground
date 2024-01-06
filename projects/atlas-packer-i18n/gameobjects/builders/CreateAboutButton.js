db85b243fe234cf6b57c35cce7fcc204e5aa49a9
Rex <binhua.tein@gmail.com>
2024-01-06 13:57:04 +0800

    Remove unused code

--- projects/atlas-packer-i18n/gameobjects/builders/CreateAboutButton.js
+++ projects/atlas-packer-i18n/gameobjects/builders/CreateAboutButton.js
@@ -63,14 +63,4 @@ var CreateAboutDialog = function (scene, configObj) {
     return dialog;
 }
 
-const AboutDialogContent = `\
-An atlas packer for phaser3 game engine.
-
-Libraries:
-- [u][url=https://github.com/photonstorm/phaser]Phaser3 game engine[/url][/u]
-- [u][url=https://rexrainbow.github.io/phaser3-rex-notes/docs/site/ui-overview/]RexUI[/url][/u]
-- [u][url=https://github.com/mapbox/potpack]potpack[/url][/u]
-
-[u][url=https://github.com/rexrainbow/phaser3-rex-playground/tree/master/projects/atlas-packer-i18n]Source code[/url][/u]`
-
 export default CreateAboutButton;
