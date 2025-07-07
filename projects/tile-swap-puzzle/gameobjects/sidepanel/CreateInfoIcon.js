eb426b815e3b1cc1f5b054a84e7514fe73d3a271
Rex <binhua.tein@gmail.com>
2025-07-08 01:08:20 +0800

    Add information modal dialog

--- projects/tile-swap-puzzle/gameobjects/sidepanel/CreateInfoIcon.js
+++ projects/tile-swap-puzzle/gameobjects/sidepanel/CreateInfoIcon.js
@@ -1,11 +1,62 @@
-import Button from '../../../../../phaser3-rex-notes/plugins/button';
+import ConfirmActionButton from '../../../../../phaser3-rex-notes/templates/ui/confirmactionbutton/ConfirmActionButton.js'
+import { COLOR_DIALOG_BG, COLOR_DIALOG_BOARD, COLOR_DIALOG_TITLE, COLOR_DIALOG_CONTENT } from '../../scenes/const.js';
 
 var CreateInfoIcon = function (scene, size) {
-    var infoIcon = scene.add.image(0, 0, 'icons', 'info').setDisplaySize(size, size);
-    new Button(infoIcon)
-        .on('click', function () {
-            // TODO
-        })
+    var dialogStyle = {
+        anchor: {
+            x: 'center', y: 'center'
+        },
+
+        width: 1000, height: 600,
+
+        space: { left: 40, right: 40, top: 40, bottom: 40, title: 40, content: 40, },
+
+        background: {
+            color: COLOR_DIALOG_BG,
+            strokeColor: COLOR_DIALOG_BOARD,
+            radius: 20,
+        },
+
+        title: {
+            text: {
+                key: 'gothic',
+                fontSize: 80,
+                tint: COLOR_DIALOG_TITLE,
+            },
+            wrapText: true
+        },
+
+        content: {
+            text: {
+                key: 'gothic',
+                fontSize: 60,
+                tint: COLOR_DIALOG_CONTENT,
+            },
+            wrapText: true,
+        },
+
+        align: {
+            title: 'left',
+            content: 'left',
+        },
+
+        buttonMode: 0,
+    };
+
+    var dialogContent = {
+        title: 'How to play',
+        content: 'Drag two tiles to swap their positions',
+    };
+
+    var infoIcon = new ConfirmActionButton(scene, {
+        icon: scene.add.image(0, 0, 'icons', 'info').setDisplaySize(size, size),
+
+        confirmDialog: {
+            style: dialogStyle,
+            content: dialogContent
+        }
+    })
+    scene.add.existing(infoIcon);
 
     return infoIcon;
 }
--- projects/tile-swap-puzzle/scenes/const.js
+++ projects/tile-swap-puzzle/scenes/const.js
@@ -1,2 +1,21 @@
 export const EASE_DURATION = 200;
 export const DATA_KEY_SCORE = 'score';
+
+export const COLOR_1000 = 0x0A0A0A;
+export const COLOR_900 = 0x121212;
+export const COLOR_800 = 0x1E1E1E;
+export const COLOR_700 = 0x2C2C2C;
+export const COLOR_600 = 0x3D3D3D;
+export const COLOR_500 = 0x5A5A5A;
+export const COLOR_400 = 0x8A8A8A;
+export const COLOR_300 = 0xB0B0B0;
+export const COLOR_200 = 0xD0D0D0;
+export const COLOR_100 = 0xFFFFF;
+
+export const COLOR_DIALOG_BG = COLOR_800;
+export const COLOR_DIALOG_BOARD = COLOR_700;
+export const COLOR_DIALOG_TITLE = COLOR_200;
+export const COLOR_DIALOG_CONTENT = COLOR_300;
+export const COLOR_DIALOG_BTN_BG = COLOR_700;
+export const COLOR_DIALOG_BTN_TXT = COLOR_100;
+export const COLOR_DIALOG_SPLIT = COLOR_600;
